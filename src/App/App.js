b8649bc8e07171f88e49a50c86bc4dedb9635a3d
Marceline Ball <marchettiball@gmail.com>
2021-06-09 21:13:07 -0600

    Add render to App

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,15 @@ class App extends Component {
     super()
     this.state = {}
   }
+
+  render() {
+    return(
+      <main>
+        <h1>Gathering the Art</h1>
+        <h2>Art so good it's Magic</h2>
+      </main>
+    )
+  }
 }
 
 export default App;
