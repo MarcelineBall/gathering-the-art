a0760de643302318533d822f304e802d263df09c
Marceline Ball <marchettiball@gmail.com>
2021-06-11 11:06:05 -0600

    Add id to mock data

--- src/mockData.js
+++ src/mockData.js
@@ -2,22 +2,26 @@ const cards = [
   {
     name: "Ancestor's Chosen",
     artist: "Aaron Aaronson",
-    imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=130550&type=card"
+    imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=130550&type=card",
+    id: "135A"
   },
   {
     name: "Sombody's Problem",
     artist: "Aaron Aaronson",
-    imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=149935&type=card"
+    imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=149935&type=card",
+    id: "245B"
   },
   {
     name: "Cthulu Fthagen",
     artist: "Howie Phil Likeskill",
-    imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=150324&type=card"
+    imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=150324&type=card",
+    id: "754G"
   },
   {
     name: "Jeezum Crow",
     artist: "Zelda Zerard",
-    imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=150422&type=card"
+    imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=150422&type=card",
+    id: "938T"
   }
 ]
 
