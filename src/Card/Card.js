a283e8c616526d9b4288ac8b2b56dfbd1b28920e
Marceline Ball <marchettiball@gmail.com>
2021-06-11 10:40:03 -0600

    Add remaining display for Card

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,7 +4,8 @@ const Card = ( {name, artist, imageUrl} ) => {
   return(
     <article>
       <img src={imageUrl} alt={name} />
-      
+      <h2>{name}</h2>
+      <button>Favorite</button>
     </article>
   )
 }
