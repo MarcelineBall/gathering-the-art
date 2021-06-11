bce03c81f6d360f76d3b89b08bb3f0a246f7afbe
Marceline Ball <marchettiball@gmail.com>
2021-06-11 17:22:44 -0600

    Add link import to Artist

--- src/Artist/Artist.js
+++ src/Artist/Artist.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Artist = ({ artists }) => {
-  return 
+  const allArtists = artists.map(artist => {
+
+  })
+  return
 }
 
 export default Artist
