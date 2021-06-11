b44ddae19f50f916b40c14b5e77861da10976760
Marceline Ball <marchettiball@gmail.com>
2021-06-11 13:16:02 -0600

    Add function to map through cards in Gallery component

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -2,6 +2,17 @@ import React from 'react'
 import Card from '../Card/Card'
 
 const Gallery = ( {cards} ) => {
+  const allCards = cards.map(card => {
+    return(
+      <Card
+        id={card.id}
+        key={card.id}
+        name={card.name}
+        artist={card.artist}
+        imageUrl={card.imageUrl}
+      />
+    )
+  })
   return <p>This is where the art will go</p>
 }
 
