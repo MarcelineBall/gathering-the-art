e184814e6a31a1ac60d7953a2413143e3e053161
Marceline Ball <marchettiball@gmail.com>
2021-06-12 10:53:35 -0600

    Refactor gallery to render a message when passed an empty array

--- src/App/App.js
+++ src/App/App.js
@@ -20,12 +20,12 @@ class App extends Component {
         <h1>Gathering the Art</h1>
         <h2>Art so good it's Magic</h2>
         <Switch>
+          <Route path='/favorites' render={() => {
+            return <Gallery cards={this.state.favorite} />
+          }} />
           <Route path='/:artist' render={({ match }) => {
             const { artist } = match.params
             return <Gallery cards={this.state.cards} />
-          }} />
-          <Route path='/favorites' render={() => {
-            return <Gallery cards={this.state.favorite} />
           }} />
           <Route exact path='/' render={() => {
             return <Artist artists={this.state.artists} />
--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -2,18 +2,22 @@ import React from 'react'
 import Card from '../Card/Card'
 
 const Gallery = ( {cards} ) => {
-  const allCards = cards.map(card => {
-    return(
-      <Card
+  if (cards) {
+    const allCards = cards.map(card => {
+      return(
+        <Card
         id={card.id}
         key={card.id}
         name={card.name}
         artist={card.artist}
         imageUrl={card.imageUrl}
-      />
-    )
-  })
-  return allCards
+        />
+      )
+    })
+    return allCards
+  } else {
+    return <h2>You have not favorited any art yet</h2>
+  }
 }
 
 export default Gallery
