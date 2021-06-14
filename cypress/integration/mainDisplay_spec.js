8fe3eeaec2119397db3374fa60711bc3aa56bc54
Marceline Ball <marchettiball@gmail.com>
2021-06-14 09:45:12 -0600

    Add tagline display test

--- cypress/integration/mainDisplay_spec.js
+++ cypress/integration/mainDisplay_spec.js
@@ -1,9 +1,13 @@
 describe('mainDisplay', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
-  }
+  })
 
   it('should display the app name on page load', () => {
     cy.get('h1').contains('Gathering the Art')
+  })
+
+  it('should display the tagline', () => {
+    cy.get('h2').contains('Art so good it\'s Magic')
   })
 })
