d2085cd274d1414e16754bc84b348c710266cd29
Marceline Ball <marchettiball@gmail.com>
2021-06-14 11:01:36 -0600

    Add intercept with stubbed data to get request

--- cypress/integration/artistDisplay_spec.js
+++ cypress/integration/artistDisplay_spec.js
@@ -1,10 +1,12 @@
 describe('artistDisplay', () => {
   beforeEach(() => {
+    cy.fixture('brandonKitkouskiCards').then((brandonKitkouskiCards) => {
+      cy.intercept('https://api.magicthegathering.io/v1/cards?artist=Brandon Kitkouski', { body: brandonKitkouskiCards.cards }).as('cards')
+    })
     cy.visit('http://localhost:3000/')
-    
   })
 
   it('should be able to visit an artist\'s page by clicking on their name', () => {
-    cy.get('p').contains('Bad Flip Productions')
+    cy.get('p').contains('Brandon Kitkouski')
   })
 })
