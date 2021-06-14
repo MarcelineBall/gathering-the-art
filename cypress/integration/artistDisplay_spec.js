c2d629845e75b41f5f1892d74497c954847b6325
Marceline Ball <marchettiball@gmail.com>
2021-06-14 12:05:57 -0600

    Refactor before each for artist display tests

--- cypress/integration/artistDisplay_spec.js
+++ cypress/integration/artistDisplay_spec.js
@@ -3,19 +3,23 @@ describe('artistDisplay', () => {
     cy.fixture('brandonKitkouskiCards').then((brandonKitkouskiCards) => {
       cy.intercept('https://api.magicthegathering.io/v1/cards?artist=Brandon_Kitkouski', brandonKitkouskiCards)
     })
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/Brandon_Kitkouski')
   })
 
   it('should be able to visit an artist\'s page by clicking on their name', () => {
-    cy.get('p').contains('Brandon Kitkouski').click()
-    cy.location('pathname').should('eq', '/Brandon_Kitkouski')
+    cy.visit('http://localhost:3000/')
+      .get('p').contains('Brandon Kitkouski').click()
+      .location('pathname').should('eq', '/Brandon_Kitkouski')
   })
 
   it('should display the cards for that artist', () => {
-    cy.visit('http://localhost:3000/Brandon_Kitkouski')
-      .get('main').children('article').should('have.length', 3)
+    cy.get('main').children('article').should('have.length', 3)
       .get('article').find('img')
       .get('.card-name').contains('Cascade Bluffs')
       .get('article').find('button')
+  })
+
+  it('should be able to go to the main page when you hit the home button', () => {
+
   })
 })
