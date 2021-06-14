8b815113b529efde93002e6293dc51df3345fdda
Marceline Ball <marchettiball@gmail.com>
2021-06-14 10:32:42 -0600

    Set up fixture file for cards for a user

--- cypress/integration/artistDisplay_spec.js
+++ cypress/integration/artistDisplay_spec.js
@@ -1,5 +1,10 @@
 describe('artistDisplay', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
+    
+  })
+
+  it('should be able to visit an artist\'s page by clicking on their name', () => {
+    cy.get('p').contains('Bad Flip Productions')
   })
 })
