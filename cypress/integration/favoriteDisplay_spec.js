51c08084c1aac5d46ab56bffa9a1fa63d67bd40d
Marceline Ball <marchettiball@gmail.com>
2021-06-14 13:30:38 -0600

    Add before each with stubbing

--- cypress/integration/favoriteDisplay_spec.js
+++ cypress/integration/favoriteDisplay_spec.js
@@ -1,3 +1,10 @@
 describe('favoriteDisplay', () => {
+  beforeEach(() => {
+    cy.fixture('brandonKitkouskiCards').then((brandonKitkouskiCards) => {
+      cy.intercept('https://api.magicthegathering.io/v1/cards?artist=Brandon_Kitkouski', brandonKitkouskiCards)
+    })
+    cy.visit('http://localhost:3000/')
+  })
+
   
 })
