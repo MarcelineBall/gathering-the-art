29802db77865c8b2e2822f63b4bf0f67afa977e9
Marceline Ball <marchettiball@gmail.com>
2021-06-14 11:29:55 -0600

    Finish visit and artist page test

--- cypress/integration/artistDisplay_spec.js
+++ cypress/integration/artistDisplay_spec.js
@@ -1,13 +1,14 @@
 describe('artistDisplay', () => {
   beforeEach(() => {
     cy.fixture('brandonKitkouskiCards').then((brandonKitkouskiCards) => {
-      cy.intercept('https://api.magicthegathering.io/v1/cards?artist=Brandon Kitkouski', brandonKitkouskiCards)
+      cy.intercept('https://api.magicthegathering.io/v1/cards?artist=Brandon_Kitkouski', brandonKitkouskiCards)
     })
     cy.visit('http://localhost:3000/')
   })
 
   it('should be able to visit an artist\'s page by clicking on their name', () => {
     cy.get('p').contains('Brandon Kitkouski').click()
+    cy.location('pathname').should('eq', '/Brandon_Kitkouski')
   })
 
 
