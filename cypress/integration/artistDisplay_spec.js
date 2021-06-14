372039139e5992e154ecad26f6fde529c258e7f3
Marceline Ball <marchettiball@gmail.com>
2021-06-14 11:18:35 -0600

    Add beginings of test for visit artist page

--- cypress/integration/artistDisplay_spec.js
+++ cypress/integration/artistDisplay_spec.js
@@ -1,14 +1,14 @@
 describe('artistDisplay', () => {
   beforeEach(() => {
     cy.fixture('brandonKitkouskiCards').then((brandonKitkouskiCards) => {
-      cy.intercept('https://api.magicthegathering.io/v1/cards?artist=Brandon Kitkouski', { body: brandonKitkouskiCards.cards }).as('cards')
+      cy.intercept('https://api.magicthegathering.io/v1/cards?artist=Brandon Kitkouski', brandonKitkouskiCards)
     })
     cy.visit('http://localhost:3000/')
   })
 
   it('should be able to visit an artist\'s page by clicking on their name', () => {
-    cy.get('p').contains('Brandon Kitkouski')
+    cy.get('p').contains('Brandon Kitkouski').click()
   })
 
-  
+
 })
